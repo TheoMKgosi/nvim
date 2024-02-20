91575e0b0f9cb2a3cb5071f18ef4ee0fe90a9868
Theo <theo.kgosiemang12@gmail.com>
2024-02-20 19:58:25 +0000

    treesitter

--- init.lua
+++ init.lua
@@ -2,3 +2,10 @@ vim.g.mapleader = " "
 
 require("lazy-manager")
 require("keymaps")
+
+vim.o.number = true
+vim.o.numberwidth = 1
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop=2
--- lua/plugins/languageserver.lua
+++ lua/plugins/languageserver.lua
@@ -17,10 +17,19 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-
+			local lspconfig = require('lspconfig')
+			lspconfig.lua_ls.setup {}
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(ev)
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_actions, opts)
+				end
+			})
 		end
+
+
 	}
 }
 
