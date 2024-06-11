48e96fc5929309e273bc9415b0010e6fc5959159
Theo <theo.kgosiemang12@gmail.com>
2024-06-11 21:18:38 +0000

    final tweak for now

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,5 +1,5 @@
 return {
-  { 
+  {
     "williamboman/mason.nvim",
     config = function ()
       require('mason').setup()
@@ -10,29 +10,54 @@ return {
     config = function ()
       require('mason-lspconfig').setup()
     end
-  },{ 
-
-    'neovim/nvim-lspconfig',
+  },{
+    "folke/neodev.nvim",
+    config = function ()
+      require('neodev').setup()
+    end
+  },{
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
+  },{
+    'neovim/nvim-lspconfig',
+    config = function ()
+      -- Set up lspconfig.
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require('lspconfig')
+      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities
+      }
+      lspconfig.tsserver.setup {
+        capabilities = capabilities
+      }
+
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+
+  },{
+    'L3MON4D3/LuaSnip',
+    dependencies = {'saadparwaiz1/cmp_luasnip','rafamadriz/friendly-snippets'}
+
+  },{
     'hrsh7th/nvim-cmp',
 
-    --For luasnip users.
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
     config = function ()
-
-      
-      -- Set up nvim-cmp.
       local cmp = require('cmp')
-
-      cmp.setup({
+      cmp.setup {
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-          end,
+            require'luasnip'.lsp_expand(args.body)
+          end
+        },
+
+        sources = cmp.config.sources{
+          { name = 'nvim_lsp'},
+          { name = 'luasnip' },
+          { name = 'buffer'}
         },
         window = {
           completion = cmp.config.window.bordered(),
@@ -42,36 +67,9 @@ return {
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true })
         }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
-        })
-      })
-      
-      -- Set up lspconfig.
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require('lspconfig')
-      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-      lspconfig.lua_ls.setup {       
-        capabilities = capabilities  
       }
-      lspconfig.tsserver.setup { 
-        capabilities = capabilities   
-      }
-      
-
-      enabled = function()
-        -- disable completion in comments
-        local context = require 'cmp.config.context'
-        -- keep command mode completion enabled when cursor is in a comment
-        if vim.api.nvim_get_mode().mode == 'c' then
-          return true
-        else
-          return not context.in_treesitter_capture("comment") 
-            and not context.in_syntax_group("Comment")
-        end
-      end
     end
-  },
+  }
 }
