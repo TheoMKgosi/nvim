37b29780b9966fed2fb12e99aa0163ef133e7bcf
Theo <theo.kgosiemang12@gmail.com>
2024-08-25 18:38:45 +0000

    fix ups

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,23 @@
 return {
-  'nvimtools/none-ls.nvim',
+  "nvimtools/none-ls.nvim",
   config = function()
-    local null_ls = require('null-ls')
+    local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua.with({
-          filetypes = { "lua"}
+          filetypes = { "lua" },
         }),
         null_ls.builtins.formatting.prettier.with({
           filetypes = { "html", "json", "yaml", "markdown" },
         }),
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.luasnip,
-
+        null_ls.builtins.formatting.black.with({
+          filetypes = { "python" },
+        }),
       },
     })
-    vim.keymap.set('n', '<leader>df', vim.lsp.buf.format, {})
-  end
-
+    vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, {})
+  end,
 }
