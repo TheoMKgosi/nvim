d456a82e675146fe5289aedaacd6de9c60c7a16b
Theo <theo.kgosiemang12@gmail.com>
2024-11-20 22:41:49 +0000

    fixed autocomplete to produce the completion I want first

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -66,9 +66,9 @@ return {
 			lspconfig.html.setup({
 				capabilities = capabilities,
 			})
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
@@ -81,23 +81,6 @@ return {
 		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
-			local ls = require("luasnip")
-
-			vim.keymap.set({ "i" }, "<C-K>", function()
-				ls.expand()
-			end, { silent = true })
-			vim.keymap.set({ "i", "s" }, "<C-L>", function()
-				ls.jump(1)
-			end, { silent = true })
-			vim.keymap.set({ "i", "s" }, "<C-J>", function()
-				ls.jump(-1)
-			end, { silent = true })
-
-			vim.keymap.set({ "i", "s" }, "<C-E>", function()
-				if ls.choice_active() then
-					ls.change_choice(1)
-				end
-			end, { silent = true })
 		end,
 	},
 	{
@@ -111,23 +94,39 @@ return {
 					end,
 				},
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
+				sources = {
 					{ name = "path" },
-					{ name = "nvim_lsp_signature_help" },
-				}),
+					{ name = "luasnip", keyword_length = 1 },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "nvim_lsp", keyword_length = 2 },
+				},
+
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
+
+				formatting = {
+					fields = { "menu", "abbr", "kind" },
+					expandable_indicator = false,
+					format = function(entry, item)
+						local menu_icon = {
+							nvim_lsp = "λ",
+							luasnip = "",
+							buffer = "Ω",
+							path = "",
+						}
+						item.menu = menu_icon[entry.source.name]
+						return item
+					end,
+				},
 			})
 		end,
 	},
