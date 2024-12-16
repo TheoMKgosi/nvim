c31614e3917967de33caf42b32a504ca0be28b1e
Theo <theo.kgosiemang12@gmail.com>
2024-12-16 21:02:30 +0000

    Revert null ls to barebones

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,9 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local nonels = require("null-ls")
+    nonels.setup({})
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua.with({
-					filetypes = { "lua" },
-				}),
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = { "html", "json", "yaml", "markdown" },
-				}),
-				null_ls.builtins.completion.luasnip,
-				null_ls.builtins.formatting.black.with({
-					filetypes = { "python" },
-				}),
-			},
-		})
-		vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, {})
+  end,
 }
